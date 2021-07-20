b9df3c8c97725352e25dc8f8cba74b6d6a9b66ff
CarolAbcl <carolallende@gmail.com>
2021-07-20 15:48:36 -0400

    includes data

--- src/components/LogoutConfirmation.jsx
+++ src/components/LogoutConfirmation.jsx
@@ -6,7 +6,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-export default function AlertDialog() {
+export default function LogoutConfirmation() {
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {

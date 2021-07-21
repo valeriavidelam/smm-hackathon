0ba257220c710126a37c6cc6a4e462b402959e32
valeriavidelam <v.videlam@gmail.com>
2021-07-21 14:41:43 -0400

    add FirebaseConfig

---
+++ firebase.js
@@ -0,0 +1,21 @@
+import app from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUh81xVcmuZpG2TbBtC-13JUCbnKusIho",
+  authDomain: "space-management-model.firebaseapp.com",
+  projectId: "space-management-model",
+  storageBucket: "space-management-model.appspot.com",
+  messagingSenderId: "84022133295",
+  appId: "1:84022133295:web:4efb5f55dbfe8b4ee83cda",
+  measurementId: "G-R3HDZTTG7X"
+};
+
+//Initialize Firebase
+app.initializeApp(firebaseConfig);
+
+const db = app.firestore()
+const auth = app.auth()
+
+export {db, auth}
